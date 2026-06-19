d958dc362f8fcff9511acea6cd6f414c26dade0d
Katherin <gr24017@ues.edu.sv>
2026-06-19 17:19:25 -0600

    uytuytnjbtvg

---
+++ Restaurante_Sabor_Gourmet/Vane/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurante_Sabor_Gourmet.Vane
+{
+    internal class Class1
+    {
+    }
+}
